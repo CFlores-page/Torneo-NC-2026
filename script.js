86fc11da62899c46fe5b766adfa7455b49383973
Cristian Flores <Cristian.flores@garzablancapreserve.com>
2026-06-11 00:52:33 -0600

    flags by country

--- script.js
+++ script.js
@@ -5,3 +5,114 @@ const dashboardData = {
   liderActual: "Marruecos",
   puntosLider: 2
 };
+
+const teams = [
+  {
+    teamId: "GER",
+    name: "Alemania",
+    unit: "BALFER",
+    flagUrl: "https://drive.google.com/uc?export=view&id=1bdZarcqh1gN8rbwTOWMf3kmM-hzI1oh_",
+    status: "ACTIVO",
+    points: 0,
+    sales: 0
+  },
+    {
+  teamId: "ARG",
+  name: "ARGENTINA",
+  unit: "PREMIER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1PMc9W1fIr5L5R8fNYHDKn_Shkh_H2Kch",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "BRA",
+  name: "BRASIL",
+  unit: "BALFER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1SGrCOcxjIDnAq_7QtErUzO7wg_qIOrK6",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "COL",
+  name: "COLOMBIA",
+  unit: "ELITE",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1L2wM1hPHu3geSRYg1MZ2q6KlgITYp_08",
+  status: "ACTIVO",
+  points: 1,
+  sales: 1
+},
+{
+  teamId: "FRA",
+  name: "FRANCIA",
+  unit: "ELITE",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1aHEfFBFwP85eXYRnvKY2gVDkWMFtg9PE",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "ESP",
+  name: "ESPAÑA",
+  unit: "LOYALTY",
+  flagUrl: "https://drive.google.com/uc?export=view&id=19NiP935osC8beYjr9_a9feinHtGfigh3",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "USA",
+  name: "ESTADOS UNIDOS",
+  unit: "ELITE",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1c9K8-XaTzPRjEYsJWR7AlfiCO9odttEx",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "NED",
+  name: "HOLANDA",
+  unit: "PREMIER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1MCv5Jkvp_Z0RP2ca5gS24EjKg8xFyEYT",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "ENG",
+  name: "INGLATERRA",
+  unit: "PREMIER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=14O9LRg1Nuf0kq5rLVnDMsvsaassAngDb",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+{
+  teamId: "MAR",
+  name: "MARRUECOS",
+  unit: "LOYALTY",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1_fcfOQBQ8Y4Gqrxyqq9UH2RzTuCs39Ij",
+  status: "ACTIVO",
+  points: 2,
+  sales: 2
+},
+{
+  teamId: "MEX",
+  name: "MEXICO",
+  unit: "PREMIER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1lCrMZOTyNsTpltjKXWbrgixgqJegzX84",
+  status: "ACTIVO",
+  points: 1,
+  sales: 1
+},
+{
+  teamId: "POR",
+  name: "PORTUGAL",
+  unit: "PREMIER",
+  flagUrl: "https://drive.google.com/uc?export=view&id=1sK08Xt5j6uc0afI5qGJ9Rl__0vczLoaA",
+  status: "ACTIVO",
+  points: 0,
+  sales: 0
+},
+]
